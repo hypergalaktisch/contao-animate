eb607fc7d19d8cac617d8b11778dfc1ad622c9b3
Georg Jaedicke <georg.jaedicke@gmail.com>
2017-08-22 23:01:35 +0200

    Dateien in neue Verzeichnisstruktur für contao4/managed-edition bringen, Plugin.php für Contao Manager anlegen

---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @copyright  Georg Jaedicke
+ * @author     Georg Jaedicke (hypergalaktisch)
+ * @package    Animate
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/hypergalaktisch/contao-animate
+ *
+ */
+namespace hypergalaktisch\AnimateBundle\ContaoManager;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+/**
+ * Plugin for the Contao Manager.
+ *
+ * @author Georg Jaedicke (hypergalaktisch)
+ */
+class Plugin implements BundlePluginInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create('hypergalaktisch\AnimateBundle\XlAnimateBundle')
+                ->setLoadAfter(['Contao\CoreBundle\ContaoCoreBundle'])
+                ->setReplace(['xl-animate']),
+        ];
+    }
+}
