2a70e52a9993ea86d7a0bb8a23bc870f3a9ab9ee
Georg Jaedicke <georg.jaedicke@gmail.com>
2017-10-25 21:42:03 +0200

    New viewportchecker, animations only FE

--- config/config.php
+++ config/config.php
@@ -1,3 +1,4 @@
 <?php
-
-$GLOBALS['TL_HOOKS']['getContentElement'][] = array('Animations', 'getContentElementWithAnimations');
+if (TL_MODE == 'FE') {
+    $GLOBALS['TL_HOOKS']['getContentElement'][] = array('Animations', 'getContentElementWithAnimations');
+}
