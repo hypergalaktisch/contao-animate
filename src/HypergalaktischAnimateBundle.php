1ae3892b93ec7c7ff28b9cfedc10f5be46c78399
hypergalaktisch <code@hypergalaktisch.com>
2019-08-15 23:24:15 +0200

    Load extension

--- src/HypergalaktischAnimateBundle.php
+++ src/HypergalaktischAnimateBundle.php
@@ -18,4 +18,26 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class HypergalaktischAnimateBundle extends Bundle
 {
+    /**
+     * Builds the bundle.
+     *
+     * It is only ever called once when the cache is empty.
+     *
+     * This method can be overridden to register compilation passes,
+     * other extensions, ...
+     *
+     * @param ContainerBuilder $container A ContainerBuilder instance
+     */
+    public function build(ContainerBuilder $container)
+    {
+        //
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function getContainerExtension()
+    {
+        return new HypergalaktischAnimateExtension();
+    }
 }
